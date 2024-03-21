a81ec307d323022355619b40e49f6becb938ea98
David Alves de Souza <alves.david@outlook.com>
2024-03-21 14:32:00 +0000

    Fix typo on Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@ const onLogout = () => {
         <router-link to="/" class="text-2xl text-white">Links4Later</router-link>
         <router-link @click="onLogout" to="/login" replace class="text-md flex items-center gap-2 text-zinc-600 py-2 px-3 rounded-lg bg-zinc-50 hover:bg-zinc-100 transition text-sm">
           <LogOut :size="16" />
-          Sign outs
+          Sign out
         </router-link>
     </header>
 </template>
